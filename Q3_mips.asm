d79d894d77f8ca97b858de2b9613f71808dad922
nandha1nks <46973715+nandha1nks@users.noreply.github.com>
2019-01-31 20:06:16 +0530

    Update Q3_mips.asm

--- Q3_mips.asm
+++ Q3_mips.asm
@@ -18,101 +18,101 @@ main:
 	.fmask	0x00000000,0
 	.set	noreorder
 	.set	nomacro
-	addiu	$sp,$sp,-456
-	sw	$31,452($sp)
-	sw	$fp,448($sp)
-	move	$fp,$sp
-	lui	$28,%hi(__gnu_local_gp)
-	addiu	$28,$28,%lo(__gnu_local_gp)
+	addiu	$sp,$sp,-456				#adds $sp and -456 and assigns to $sp(allocating space for intergers 57
+	sw	$31,452($sp)				#stores $31 to $sp+452
+	sw	$fp,448($sp)				#stores $fp to $sp+448
+	move	$fp,$sp					#moves $sp to $fp
+	lui	$28,%hi(__gnu_local_gp)			#stores %hi(__gnu_local_gp)<<16 into $28
+	addiu	$28,$28,%lo(__gnu_local_gp)		#unsigned adds %lo(__gnu_local_gp)+$28 to $28
 	.cprestore	16
-	lw	$2,%got(__stack_chk_guard)($28)
-	lw	$2,0($2)
-	sw	$2,444($fp)
-	li	$2,2			# 0x2
-	sw	$2,28($fp)
-	sw	$0,36($fp)
-	b	$L2
-	nop
+	lw	$2,%got(__stack_chk_guard)($28)		#loads %got(__stack_chk_guard)($28) to $2
+	lw	$2,0($2)				#loads $2+0 to $2
+	sw	$2,444($fp)				#stores $2 in $fp+444
+	li	$2,2			# 0x2		#assigns 2 to $2
+	sw	$2,28($fp)				#stores $2 in $fp+28
+	sw	$0,36($fp)				#stores $0 in $fp+36
+	b	$L2					#unconditionally branch to Label2
+	nop						#does nothing
 
 $L8:
-	sw	$0,40($fp)
-	li	$2,2			# 0x2
-	sw	$2,32($fp)
-	b	$L3
-	nop
+	sw	$0,40($fp)				#stores $0 in $fp+40
+	li	$2,2			# 0x2		#assigns 2 in $2
+	sw	$2,32($fp)				#stores $2 in $fp+32
+	b	$L3					#unconditionally branch to Label3
+	nop						#does nothing
 
 $L6:
-	lw	$3,28($fp)
-	lw	$2,32($fp)
-	teq	$2,$0,7
-	div	$0,$3,$2
-	mfhi	$2
-	bne	$2,$0,$L4
-	nop
+	lw	$3,28($fp)				#loads $fp+28 to $3
+	lw	$2,32($fp)				#loads $fp+32 to $2
+	teq	$2,$0,7					#checks whether it is an divide by zero error
+	div	$0,$3,$2				#divides the values in $3 and $2 and stores it in $0
+	mfhi	$2					#hi contains the remainder of the divison and stores this value in $2
+	bne	$2,$0,$L4				#if($2!=$0) branches to Label4
+	nop						#does nothing
 
-	li	$2,1			# 0x1
-	sw	$2,40($fp)
-	b	$L5
-	nop
+	li	$2,1			# 0x1		#assigns 1 to $2
+	sw	$2,40($fp)				#stores $2 in $fp+40
+	b	$L5					#unconditionally branches to Label5
+	nop						#does nothing
 
 $L4:
-	lw	$2,32($fp)
-	addiu	$2,$2,1
-	sw	$2,32($fp)
+	lw	$2,32($fp)				#stores $fp+32 in $2
+	addiu	$2,$2,1					#adds $2+1 to $2
+	sw	$2,32($fp)				#stores $2 to $sp+32
 $L3:
-	lw	$2,28($fp)
-	srl	$3,$2,31
-	addu	$2,$3,$2
-	sra	$2,$2,1
-	move	$3,$2
-	lw	$2,32($fp)
-	slt	$2,$2,$3
-	bne	$2,$0,$L6
-	nop
+	lw	$2,28($fp)				#loads $fp+28 to $2
+	srl	$3,$2,31				#stores $2+32 to $3
+	addu	$2,$3,$2				#stores $2 + $3 to $2
+	sra	$2,$2,1					#stores $2>>1 to $2
+	move	$3,$2					#moves $2 to $3
+	lw	$2,32($fp)				#loads $fp+32 to $2
+	slt	$2,$2,$3				#if($2<$3) then $2 = 1, $2 =0
+	bne	$2,$0,$L6				#if($2!=$0) jump to Label6
+	nop						#does nothing
 
 $L5:
-	lw	$2,40($fp)
-	bne	$2,$0,$L7
-	nop
+	lw	$2,40($fp)				#loads $2 with $fp+40
+	bne	$2,$0,$L7				#if($2!=$0) then branches to Label7
+	nop						#does nothing
 
-	lw	$2,36($fp)
-	addiu	$3,$2,1
-	sw	$3,36($fp)
-	sll	$2,$2,2
-	addiu	$3,$fp,448
-	addu	$2,$3,$2
-	lw	$3,28($fp)
-	sw	$3,-404($2)
+	lw	$2,36($fp)				#loads $fp+36 to $2
+	addiu	$3,$2,1					#adds $2+1 to $3
+	sw	$3,36($fp)				#stores $3 to $fp+36
+	sll	$2,$2,2					#stores $2<<2 to $2
+	addiu	$3,$fp,448				#stores $fp + 448 to $3
+	addu	$2,$3,$2				#stores $2 + $3 to $2
+	lw	$3,28($fp)				#stores $fp+28 to $3
+	sw	$3,-404($2)				#stores $2-404 to $3
 $L7:
-	lw	$2,28($fp)
-	addiu	$2,$2,1
-	sw	$2,28($fp)
+	lw	$2,28($fp)				#loads $fp+28 to $2
+	addiu	$2,$2,1					#stores $2 + 1 to $2
+	sw	$2,28($fp)				#stores $2 to $fp+28
 $L2:
-	lw	$2,28($fp)
-	slt	$2,$2,101
-	bne	$2,$0,$L8
-	nop
+	lw	$2,28($fp)				#loads $fp+28 to $2
+	slt	$2,$2,101				#if($2<101) then $2 = 1, then $2=0
+	bne	$2,$0,$L8				#if($2!=$0) then branches to Label8
+	nop						#does nothing
 
-	move	$2,$0
-	lw	$3,%got(__stack_chk_guard)($28)
-	lw	$4,444($fp)
-	lw	$3,0($3)
-	beq	$4,$3,$L10
-	nop
+	move	$2,$0					#moves $2 to $0
+	lw	$3,%got(__stack_chk_guard)($28)		#loads $3 with %got(__stack_chk_guard)($28)
+	lw	$4,444($fp)				#loads $4 with $fp+444
+	lw	$3,0($3)				#loads $3 with $3+0
+	beq	$4,$3,$L10				#if($4==$3) branches to Label10
+	nop						#does nothing
 
-	lw	$2,%call16(__stack_chk_fail)($28)
-	move	$25,$2
-	.reloc	1f,R_MIPS_JALR,__stack_chk_fail
-1:	jalr	$25
-	nop
+	lw	$2,%call16(__stack_chk_fail)($28)	#$loads $2 with %got(__stack_chk_guard)($28)
+	move	$25,$2					#moves $25 with $2
+	.reloc	1f,R_MIPS_JALR,__stack_chk_fail		
+1:	jalr	$25					#jumps to subroutine $25
+	nop						#does nothing
 
 $L10:
-	move	$sp,$fp
-	lw	$31,452($sp)
-	lw	$fp,448($sp)
-	addiu	$sp,$sp,456
-	j	$31
-	nop
+	move	$sp,$fp					#moves $sp to $fp
+	lw	$31,452($sp)				#loads $31 with $sp+452
+	lw	$fp,448($sp)				#loads $fp with $sp+448   #dereferencing $fp
+	addiu	$sp,$sp,456				#loads $sp + 448 with $sp
+	j	$31					#loads $31
+	nop						#does nothing
 
 	.set	macro
 	.set	reorder
